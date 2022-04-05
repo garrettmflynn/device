8c97309769dd0ef324a1594f2c4a1b123c8754ed
Garrett <gflynn@usc.edu>
2022-04-05 10:36:55 -0700

    Removed protocol-specific info

--- src/index.ts
+++ src/index.ts
@@ -17,24 +17,28 @@ const device = {
 
     // ----------------- Device Protocols -----------------
 
-    // Bluetooth
+    // ----------------- Bluetooth -----------------
     // namePrefix: 'HEG', // Filter for specified name from Bluetooth results (required if device should be selectable with a loose constraint (e.g. {bluetooth: true}))
     // serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
-    characteristics: {
-        transmit: '6e400003-b5a3-f393-e0a9-e50e24dcca9e',
-        receive: '6e400002-b5a3-f393-e0a9-e50e24dcca9e',
-    }, 
+    // characteristics: {
+    //     transmit: '6e400003-b5a3-f393-e0a9-e50e24dcca9e',
+    //     receive: '6e400002-b5a3-f393-e0a9-e50e24dcca9e',
+    // }, 
 
-    // Serial / USB
-    usbVendorId: 4292,
-    usbProductId: 60000,
-    bufferSize: 1000,
-    baudRate: 115200,
+    // ----------------- Serial / USB -----------------
+    // usbVendorId: 4292,
+    // usbProductId: 60000,
+    // bufferSize: 1000,
+    // baudRate: 115200,
 
-    // WebSocket / Wifi
-    url: 'https://localhost',
+    // ----------------- WebSocket / Wifi -----------------
+    // url: 'https://localhost',
 
-    protocols: ['serial', 'bluetooth', 'websocket']
+    protocols: [
+        // 'serial', 
+        // 'bluetooth', 
+        // 'websocket'
+    ]
 
 }
 
